656564628d7facfd794c48431ebcdd2f4bf02d9f
gedesurya125 <gedesurya125@gmail.com>
2023-06-01 23:02:03 +0800

    setup surya ui package

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import 'styles/globals.css';
 
+import { ThemeProvider } from '@gedesurya125/surya-ui';
+
+import { theme } from 'theme';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
---
+++ src/theme/index.js
@@ -0,0 +1,7 @@
+export const theme = {
+  fonts: {
+    body: 'sans-serif',
+    heading: 'sans-serif',
+    monospace: 'sans-serif',
+  },
+};
